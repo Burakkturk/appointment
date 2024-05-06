37a832b37c6bab95f918df2782af3fb5030c4286
Burakkturk <burak1845turk@icloud.com>
2024-05-06 13:08:27 +0300

    feat: consulted toggle mechanism added

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -6,17 +6,29 @@ const AppointmentList = ({ apps, setApps }) => {
   console.log(apps);
 
   const handleDelete = (id) => {
-    setApps ([])
+    setApps(apps.filter((item) => item.id !== id));
+  }
+
+    const handleDoubleClick = (id) => {
+      setApps(
+        apps.map((item) =>
+          item.id == id ? { ...item, consulted: !item.consulted } : item
+        )
+      );
+    
   };
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
         Appointment List
       </h3>
+      {apps.length < 1 && <img src="./img/appointment.jpg" width="70%" />}
+
       {apps.map(({ id, patient, consulted, doctor, day }) => (
         <div
           key={id}
           className={consulted ? "appointments consulted" : "appointments"}
+          onDoubleClick={() => handleDoubleClick(id)}
         >
           <Row className="justify-content-between align-item-center">
             <Col xs={12} sm={12} md={6}>
@@ -31,7 +43,7 @@ const AppointmentList = ({ apps, setApps }) => {
               <TiDelete
                 className="text-danger fs-1"
                 type="button"
-                onClick={() => handleDelete (id) }
+                onClick={() => handleDelete(id)}
               />
             </Col>
           </Row>
